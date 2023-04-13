cf1d3ffad97d04f68718b71a96fc913d7f4f95dc
itamar hess <Itamardarihess@gmail.com>
2023-04-13 18:33:31 +0300

    new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import HomePage from "./pages/home-page";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 function App() {
 
   return (
     <div className="App">
-      <HomePage/>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<HomePage/>}/>
+         </Routes>
+      </BrowserRouter>
     </div>
   )
 }
