8d10e782b2ae8d6597550173f0630c180f442e63
itamar hess <Itamardarihess@gmail.com>
2023-04-13 18:41:43 +0300

    code stracture update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import About from "./components/about";
 import HomePage from "./pages/home-page";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
          <Routes>
             <Route path="/" element={<HomePage/>}/>
+            <Route path="*" element={<About/>}/>
          </Routes>
       </BrowserRouter>
     </div>
